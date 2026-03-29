9a6b3cc8e8ee4b524066d251f3257c188bf8f2c5
Madelavishnu <madelavishnu40@gmail.com>
2026-03-29 09:42:42 +0530

    Add files via upload

---
+++ feedmcp.py
@@ -0,0 +1,30 @@
+from fastmcp import FastMCP
+
+mcp = FastMCP(name ="calculator")
+
+@mcp.tool()
+def multiply(a:float,b:float) -> float:
+    """Multiply two numbers.
+    
+    args: a (float): The first number.
+          b (float): The second number.
+          
+    returns: float: The product of the two numbers.
+    """
+    return a*b
+
+
+@mcp.tool()
+def add(a:float,b:float) -> float:
+    """Add two numbers.
+    
+    args: a (float): The first number.
+          b (float): The second number.
+          
+    returns: float: The sum of the two numbers.
+    """
+    return a+b
+
+if __name__ == "__main__":
+    
+    mcp.run(transport="http", host = "localhost",port = 8003)
